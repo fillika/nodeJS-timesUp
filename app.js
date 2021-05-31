b3baa982447bf20c939838b02840178e4a241c20
Aleksandr M <alfie.mir@gmail.com>
2021-05-31 16:30:10 +0300

    Добавил cors библиотеку. Добавил origin.

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-
+const { tasks } = require("./mock-data/tasks");
+const cors = require("cors");
 const app = express();
 
 const serverSettings = {
@@ -9,9 +10,21 @@ const serverSettings = {
   host: "127.0.0.1",
 };
 
-const page404 = fs.readFileSync(path.join(__dirname, "/public/404.html"), 'utf8');
+const page404 = fs.readFileSync(
+  path.join(__dirname, "/public/404.html"),
+  "utf8"
+);
 
+var corsOptions = {
+  origin: 'http://127.0.0.1:5500',
+}
+
+app.use(cors());
 app.use(express.static("public"));
+
+app.post("/api/tasks/", cors(corsOptions), function (req, res) {
+  res.json(tasks);
+});
 
 app.use("*", (req, res) => {
   res.status(404).send(page404);
