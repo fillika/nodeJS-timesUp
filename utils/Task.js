405bcb9dd2cf0e1a65b8ba156166ebc549d189ff
Aleksandr M <alfie.mir@gmail.com>
2021-06-24 13:54:41 +0300

    Доработал алгоритм, который позволяет парсить один большой длинный таск на мелкие. Пофиксил небольшой баг с пересчетом финального куска времени, который не учитывал начального куска времени

--- utils/Task.js
+++ utils/Task.js
@@ -14,17 +14,7 @@ class Task {
   }
 
   createTaskListFromLongDate(data, start, duration, result) {
-    const startDay = {
-      day: new Date(start).getDate(),
-      month: new Date(start).getMonth() + 1,
-      year: new Date(start).getFullYear(),
-    };
-
-    const currentDate = `${startDay.year}-${startDay.month}-${startDay.day}`;
-
-    const currentDayInMs = new Date(currentDate).getTime(); // Нахожу начало текущего дня
-    const nextDayInMS = currentDayInMs + this.oneDayLength; // Нахожу начало следующего дня
-    const restOfCurrentDay = nextDayInMS - new Date(start).getTime(); // Это остаток для текущего дня у задачи.
+    const { nextDayInMS, restOfCurrentDay } = this.countNextDayAndRest(start);
 
     result.push({
       ...data,
@@ -41,15 +31,37 @@ class Task {
       this.createTaskListFromLongDate(data, start, duration, result);
     } else {
       start = nextDayInMS;
+      // Тут Я еще раз вызываю функцию отдельно. Потому что до этого мы вычитали полное кол-во времени, не учитывая 
+      // Самого первого кусочка времени, которое осталось в самой первой половине таска
+      const oldData = this.countNextDayAndRest(data.start);
 
       result.push({
         ...data,
         start: start,
         stop: new Date(data.stop).getTime(),
         at: new Date(data.stop).getTime() + 1000,
-        duration: duration + this.oneDayLength,
+        duration: duration + this.oneDayLength - oldData.restOfCurrentDay,
       });
     }
+  }
+
+  countNextDayAndRest(start) {
+    const startDay = {
+      day: new Date(start).getDate(),
+      month: new Date(start).getMonth() + 1,
+      year: new Date(start).getFullYear(),
+    };
+
+    const currentDate = `${startDay.year}-${startDay.month}-${startDay.day}`;
+
+    const currentDayInMs = new Date(currentDate).getTime(); // Нахожу начало текущего дня
+    const nextDayInMS = currentDayInMs + this.oneDayLength; // Нахожу начало следующего дня
+    const restOfCurrentDay = nextDayInMS - new Date(start).getTime(); // Это остаток для текущего дня у задачи.
+
+    return {
+      nextDayInMS,
+      restOfCurrentDay,
+    };
   }
 }
 
