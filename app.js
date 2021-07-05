ca025d9a74d39d911083cb95b1dd2e633e94cf49
Aleksandr M <alfie.mir@gmail.com>
2021-07-05 19:52:12 +0300

    Добавил обработку всех гет запросов для корректной работы react router

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config({ path: `${__dirname}/config.env` });
 
 const express = require("express");
+const path = require('path');
 const cors = require("cors");
 const app = express();
 const connectionDB = require("./dbConnection");
@@ -12,11 +13,15 @@ const globalErrorHandler = require("./controllers/errorController.js");
 
 connectionDB();
 
+var allowList = ["http://127.0.0.1:5500", "http://localhost:3000/", "http://localhost:5500/"];
+
 var corsOptions = {
-  origin: "http://127.0.0.1:5500",
+  // origin: "http://127.0.0.1:5500",
+  origin: allowList,
   // methods: "POST",
   // allowedHeaders: "application/json",
 };
+
 
 app.use(cors());
 app.use(express.json());
@@ -25,6 +30,10 @@ app.use(express.static("public"));
 app.use("/api/v1/tasks", cors(corsOptions), taskRouter);
 app.use("/api/v1/activeTask", cors(corsOptions), activeTaskRouter);
 app.use("/api/v1", cors(corsOptions), authRouter);
+
+app.get('*', cors(corsOptions), (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl}`, 404));
